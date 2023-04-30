415225bd7423984b692c8ee35ee88b0fccb48426
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2023-04-30 21:58:42 +0100

    Tidy up

--- devenv.nix
+++ devenv.nix
@@ -2,21 +2,20 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env.GREET = "DevEnv has been loaded";
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git ];
 
   enterShell = ''
     hello
-    git --version
   '';
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+  scripts.hello.exec = "echo $GREET";
 
   # https://devenv.sh/pre-commit-hooks/
   pre-commit.hooks = {
